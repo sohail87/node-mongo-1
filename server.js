25ff5f5f729d26c408a588fe31b6f3f6001a76d4
sohail87 <sohaildeen73@gmail.com>
2016-12-05 20:32:54 +0000

    accepts json body
    connected to mongo db

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 'use strict';
 
 const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const mongodbUriUtil = require('mongodb-uri');
+const config = require('./config.json');
+
 let contacts = require('./data');
 
+const app = express();
+
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
+
+const db = config.databaseConnection;
+const mongodbUri = `mongodb://${db.username}:${db.password}@${db.host}:${db.port}/${db.database}`;
+const mongooseUri = mongodbUriUtil.formatMongoose(mongodbUri);
+const dbOptions = {};
 
 app.get('/api/contacts',(req, res) => {
   if(!contacts){
@@ -78,5 +89,10 @@ const hostname = 'localhost'
 const port = 3001;
 
 app.listen(port, hostname, () => {
-  console.log(`Server is running at http://${hostname}:${port}`);
+  mongoose.connect(mongooseUri, dbOptions, (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
 });
