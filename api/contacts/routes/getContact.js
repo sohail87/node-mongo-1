c3fd3573331288d33368fb7eb1da2cbc37f94cb5
sohail87 <sohaildeen73@gmail.com>
2017-03-07 16:44:40 +0000

    testing commit from work
    (cherry picked from commit c5c67587cbecf79151f92c93912c6a9391167826)

--- api/contacts/routes/getContact.js
+++ api/contacts/routes/getContact.js
@@ -9,9 +9,11 @@ router.route('/:id')
     const _id = req.params.id;
 
     Contact.findOne({ _id }, (err, contact) => {
+		
       if (err) {
         res.status(400).json(err);
       }
+	  
       if (!contact) {
         res.status(404).json({ message: 'Contact not found.' });
       }
